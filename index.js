5a603f6564f90ba02f7613042fdb1e4397f56611
lanath <valentin.vivier@bhc-it.com>
2019-10-01 21:33:02 +0200

    exposed more

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import bhc, {getUri, getQuery} from './src/bhcAuthWrapper'
-import Query from './query'
-import User from './user'
-import XMLHttpRequestAsync from './httpWrapper'
+import Query from './src/query'
+import User from './src/user'
+import XMLHttpRequestAsync from './src/httpWrapper'
 
 export default bhc
 
